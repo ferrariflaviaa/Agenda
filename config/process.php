0b228d9b34c9f961943acc2c487be116ebb25447
Flávia Ferrari <flaviaferrarit@gmail.com>
2022-09-28 08:42:58 -0300

    feat: creating home page

--- config/process.php
+++ config/process.php
@@ -4,6 +4,7 @@
   include_once("connection.php");
   include_once("url.php");
 
+  $contacts = [];
   $query = "SELECT * FROM contacts";
 
   $stmt = $conn -> prepare($query);
--- index.php
+++ index.php
@@ -1,6 +1,38 @@
 <?php
-  include_once("templates/header.php");
+include_once("templates/header.php");
 ?>
 
-<!-- <div class="container">
-</div> -->
+<div class="container">
+  <?php if (isset($printMsg) && $printMsg != "") : ?>
+    <p id="msg"><?= $printMsg ?></p>
+  <?php endif; ?>
+  <h1 id="main-title">Minha agenda</h1>
+  <?php if (count($contacts) > 0) : ?>
+    <table class="table" id="contacts-table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Nome</th>
+          <th scope="col">Telefone</th>
+          <th scope="col"></th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php foreach($contacts as $contacts):?>
+          <tr>
+            <th scope="row"><?=  $contacts["id"]?></th>
+            <td><?=$contacts["name"]?></td>
+            <td><?=$contacts["phone"]?></td>
+            <td class="actions">
+              <a href="<?=$BASE_URL?>show.php? id=<?= $contacts['id']?>"><i class="fas fa-eye check-icon"></i></a>
+              <a href="<?=$BASE_URL?>edit.php? id=<?= $contacts['id']?>"><i class="far fa-edit edit-icon"></i></a>
+              <button type="submit"><i class="fas fa-times delete-icon"></i></button> 
+          </td>
+          </tr>
+          <?php endforeach; ?>
+      </tbody>
+    </table>
+  <?php else : ?>
+    <p id="empty-list-text">Ainda não há contatos na sua agenda, <a href="<?= $BASE_URL ?> create.php">clique aqui para adicionar</a>.</p>
+  <?php endif; ?>
+</div>
