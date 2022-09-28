9ab7452a382959fa4c142333b560ef2363cbaef1
Flávia Ferrari <flaviaferrarit@gmail.com>
2022-09-28 14:07:55 -0300

    feat: add contact to the phonebook

--- config/process.php
+++ config/process.php
@@ -1,27 +1,54 @@
-<?php 
-  session_start();
+<?php
+session_start();
 
-  include_once("connection.php");
-  include_once("url.php");
+include_once("connection.php");
+include_once("url.php");
+
+$data = $_POST;
+
+if ((!empty($data))) {
+  //CRIAR CONTATO:
+  if ($data["type"] === "create") {
+    $name = $data["name"];
+    $phone = $data["phone"];
+    $observations = $data["observations"];
+
+    $query = "INSERT INTO contacts (name, phone, observations) VALUES (:name, :phone, :observations)";
+
+    $stmt = $conn->prepare($query);
+
+    $stmt->bindParam(":name", $name);
+    $stmt->bindParam(":phone", $phone);
+    $stmt->bindParam(":observations", $observations);
+
+    try {
+      $stmt->execute();
+      $_SESSION["msg"] = "Contato adicionado com sucesso!";
+    } catch (PDOException $e) {
+      //VERIFICAR ERRO:
+      $erro = $e->getMessage();
+      echo "Erro: $erro";
+    }
+  }
+  header("Location: " . $BASE_URL . "../index.php");
+  
+} else {
 
   $id;
 
-  if(!empty($_GET)){
+  if (!empty($_GET)) {
     $id = $_GET["id"];
   }
 
   //Retorna dado de um post específico
-  if(!empty($id)){
+  if (!empty($id)) {
     $query = "SELECT * FROM contacts WHERE id = :id";
 
     $stmt = $conn->prepare($query);
     $stmt->bindParam(":id", $id);
     $stmt->execute();
     $contact = $stmt->fetch();
-
-    
-
-  }else{
+  } else {
     //Retorna todos os contatos
     $query = "SELECT * FROM contacts";
     $stmt = $conn->prepare($query);
@@ -35,8 +62,9 @@
 
   $query = "SELECT * FROM contacts";
 
-  $stmt = $conn -> prepare($query);
+  $stmt = $conn->prepare($query);
 
-  $stmt-> execute();
+  $stmt->execute();
 
   $contacts = $stmt->fetchAll();
+}
